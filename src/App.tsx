12d4d9734bb1dac15610be2ee58bb8c89a8e721e
Hi, I'm Victor Joseph <okevictor14@gmail.com>
2025-04-08 19:54:21 +0300

    Setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <div className='text-3xl font-bold text-center'>Vikkymedia Technology</div>
+    </>
+  )
+}
+
+export default App
